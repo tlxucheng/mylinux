92eb4b23482ec8a0faca98eab48d59e5deda2d9e
tlxucheng <15850795746@163.com>
2019-08-18 21:16:06 +0800

    add test_mul3.c for b register usage

---
+++ App/test_asm/att/test_mul3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 0;
+    int c = 2;
+
+    __asm__("imull %1, %2\n\t"
+        "movl %2, %0"
+	:"=b"(b)
+	:"r"(a), "r"(c)
+    );
+
+    printf("result: %d\n", b);
+
+    return 0;
+}

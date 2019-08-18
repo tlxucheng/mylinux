5ff1d1ff45b1a42eb4639d0caba01d45f7e0bfa7
tlxucheng <15850795746@163.com>
2019-08-18 21:07:21 +0800

    add test_mul2.c for 'clobbered_regs' usage

---
+++ App/test_asm/att/test_mul2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 0;
+    int c = 2;
+
+    __asm__("imull %1, %2\n\t"
+        "movl %2, %0"
+	:"=r"(b)
+	:"r"(a), "r"(c)
+        :"eax"	
+    );
+
+    printf("result: %d\n", b);
+
+    return 0;
+}

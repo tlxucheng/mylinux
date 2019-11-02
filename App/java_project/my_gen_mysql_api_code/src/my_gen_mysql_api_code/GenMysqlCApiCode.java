d16867a2add204b4a340808df2007f9988e60564
tlxucheng <15850795746@163.com>
2019-11-02 14:45:21 +0800

    gen source and head first line function

--- App/java_project/my_gen_mysql_api_code/src/my_gen_mysql_api_code/GenMysqlCApiCode.java
+++ App/java_project/my_gen_mysql_api_code/src/my_gen_mysql_api_code/GenMysqlCApiCode.java
@@ -9,7 +9,7 @@ import jxl.Sheet;
 import jxl.Workbook;
 
 class FileHandle {
-	void WirteFile(String m_file_name, String m_content) throws Exception {
+	void WriteFile(String m_file_name, String m_content) throws Exception {
 		File file = new File(m_file_name);
 		
 		if(!file.exists()){
@@ -28,6 +28,55 @@ class FileHandle {
 	}
 }
 
+class CFileHandle extends FileHandle {
+	private String m_table_name;
+	
+	public void SetTableName(String table_name) {
+		m_table_name = table_name;		
+	}
+	
+	public String GenHeadFileName() {
+		return m_table_name + ".h";
+	}
+	
+	public String GenSourceFileName() {
+		return m_table_name + ".c";
+	}
+	
+	public void WriteHeadFile() throws Exception {
+		FileHandle filehandle = new FileHandle();
+		String     head_file_name  = GenHeadFileName();
+		String     content = "#include <stdio.h>";
+		
+		filehandle.WriteFile(head_file_name, content);
+	}	
+	
+	public void WriteSourceFile() throws Exception {
+		FileHandle filehandle = new FileHandle();
+		String     head_file_name  = GenHeadFileName();
+		String     source_file_name  = GenSourceFileName();
+		String     content = "#include <" + head_file_name + ">";
+		
+		filehandle.WriteFile(source_file_name, content);
+	}
+}
+
+class CppFileHandle extends FileHandle {
+	
+}
+
+class JavaFileHandle extends FileHandle {
+	
+}
+
+class NodeJsFileHandle extends FileHandle {
+	
+}
+
+class PhpFileHandle extends FileHandle {
+	
+}
+
 class ExcelHandle {
 	void ReadExcel() throws Exception {
 		Workbook workbook = Workbook.getWorkbook(new File("create_table_sql.xls"));
@@ -41,14 +90,13 @@ class ExcelHandle {
 		String m_file_name = cell.getContents();
 		System.out.println(m_file_name);
 		
-		FileHandle filehandle = new FileHandle();
 		
-		String c_file = m_file_name+".c";;
-		String h_file = m_file_name+".h";;
+		CFileHandle cfilehandle = new CFileHandle();
 		
-		filehandle.WirteFile(c_file, "hello world!");
-		filehandle.WirteFile(h_file, "hello world!");
-
+		cfilehandle.SetTableName(m_file_name);
+		cfilehandle.WriteHeadFile();
+		cfilehandle.WriteSourceFile();
+		
 		workbook.close();
 	}
 	

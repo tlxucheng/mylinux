e60c22f1bccf1e5294d1de0175e3e815050b3eb7
tlxucheng <15850795746@163.com>
2020-09-13 15:52:17 +0800

    add Strategy folder

---
+++ App/cpp_project/design_patterns/Strategy/Traditional/Strategy.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+class Databases
+{
+public:
+    virtual void connect() = 0;
+};
+
+class MysqlDb:public Databases
+{
+public:
+    void connect() override
+    {
+        cout << "MysqlDb Class connect" << endl;
+    }
+};
+
+class OracleDb:public Databases
+{
+public:
+    void connect() override
+    {
+        cout << "OracleDb Class connect" << endl;
+    }
+};
+
+template<typename T>
+class callmodel
+{
+public:
+    void getdata()
+    {
+        m_db.connect();
+    }
+
+private:
+    T m_db;
+};
+
+int main()
+{
+    callmodel<MysqlDb> *test_mysqldb = new callmodel<MysqlDb>;
+
+    test_mysqldb->getdata();
+    delete test_mysqldb;
+    test_mysqldb = nullptr;
+
+    callmodel<OracleDb> *test_oracle_db = new callmodel<OracleDb>;
+
+    test_oracle_db->getdata();
+    delete test_oracle_db;
+    test_oracle_db = nullptr; 
+    
+    return 0;
+}
+
+

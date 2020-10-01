dea42f1121f252881f88553d89d822c420e93d56
tlxucheng <15850795746@163.com>
2020-10-01 21:34:04 +0800

    add nursing_artifact.sql

---
+++ App/php_project/ThinkPHP_full_v5.0.24/sql/nursing_artifact.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `nursing_artifact`;
+CREATE TABLE `nursing_artifact` (
+`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增长id',
+`project_type` tinyint(4) unsigned NOT NULL DEFAULT '0' COMMENT '项目：0-母乳；1-配方奶；2-开水；3-便便',
+`project_number` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '项目的数量或者次数',
+`project_data` DATE NULL DEFAULT NULL COMMENT '项目日期',
+`project_time` TIME NULL DEFAULT NULL COMMENT '项目时间',
+`project_comments` varchar(2048) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '项目备注',
+`create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+`update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+PRIMARY KEY (`id`),
+UNIQUE KEY `tdt`(`project_type`, `project_data`, `project_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='喂奶神器表';
+
+
+INSERT INTO `nursing_artifact` VALUES ('1', 1, '100', '2020-10-01', '21:15', '', '1601558121', '1601558121');

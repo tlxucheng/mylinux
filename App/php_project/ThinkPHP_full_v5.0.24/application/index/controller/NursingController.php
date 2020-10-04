4f137b6eba93d698e4963ea9f3a41765c9e6e17f
tlxucheng <15850795746@163.com>
2020-10-04 18:56:36 +0800

    add update history table

--- App/php_project/ThinkPHP_full_v5.0.24/application/index/controller/NursingController.php
+++ App/php_project/ThinkPHP_full_v5.0.24/application/index/controller/NursingController.php
@@ -3,6 +3,7 @@ namespace app\index\controller;
 use think\Controller;
 use think\Request;
 use app\common\model\Nursing;
+use app\common\model\History;
 
 class NursingController extends Controller
 {
@@ -32,7 +33,27 @@ class NursingController extends Controller
 		
 		$Nursing->save();
 		
-		return '新增成功。新增ID为:' . $Nursing->id;
+		$History = new History();
+		$historys = $History->where('project_date',$Nursing->project_date)->select();
+		
+		$History->project_date = $postdata['project_date'];
+		if(!empty($historys))
+	    {
+			$History->breast_milk_number = $historys[0]->breast_milk_number + $postdata['project_number'];
+			
+			$update_array = [
+				'project_date' => $History->project_date,
+				'breast_milk_number' => $History->breast_milk_number
+		    ];
+			$History->save($update_array, ['project_date' => $postdata['project_date']]);
+	    }
+		else
+	    {
+	        $History->breast_milk_number = $postdata['project_number'];
+			$History->save();
+		}
+
+		return '新增成功';
 	}
 	
 	public function statistics()
